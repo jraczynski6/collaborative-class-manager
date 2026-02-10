7e3aed4e757802a6537c737b1137b0d5b34989aa
BrunsAraujo <bruno.correadearaujo@gmail.com>
2026-02-09 18:42:16 -0600

    Create Teacher model

---
+++ src/main/java/model/Teacher.java
@@ -0,0 +1,57 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+
+
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstname;
+    private String lastname;
+    private String subject;
+
+    public Teacher() {}
+
+    public Teacher(String firstname, String lastname, String subject) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.subject = subject;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
