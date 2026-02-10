6a4575a407f2ba57e1f21eea469424aaf9e4ee68
Joseph R <raczynski.joseph616@gmail.com>
2026-02-09 18:25:55 -0600

    test commit

--- src/main/java/com/example/collaborative_class_manager/CollaborativeClassManagerApplication.java
+++ src/main/java/com/example/collaborative_class_manager/CollaborativeClassManagerApplication.java
@@ -8,6 +8,9 @@ public class CollaborativeClassManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollaborativeClassManagerApplication.class, args);
+
+
+
 	}
 
 }
