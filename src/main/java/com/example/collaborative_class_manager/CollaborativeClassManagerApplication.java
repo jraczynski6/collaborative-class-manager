89c14db3ff47bf8f7372b956cb163bfd250f13c7
Joseph R <raczynski.joseph616@gmail.com>
2026-02-09 18:23:29 -0600

    Initial Commit

---
+++ src/main/java/com/example/collaborative_class_manager/CollaborativeClassManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.collaborative_class_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeClassManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeClassManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/collaborative_class_manager/CollaborativeClassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collaborative_class_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeClassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
